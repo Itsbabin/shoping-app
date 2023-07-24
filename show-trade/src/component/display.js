2bdad8a7ac7a1686dab08e4aa6b40a420b5bc7a9
Biswajit Mandal <itsbabin2004@gmail.com>
2023-07-25 01:31:02 +0530

    initial starting

---
+++ show-trade/src/component/display.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Display() {
+  return (
+    <>
+    <div className="display">
+        <div className="candlestick">
+            <div className="candle"></div>
+            <div className="stick"> </div>
+        </div>
+    </div>
+    </>
+  )
+}
